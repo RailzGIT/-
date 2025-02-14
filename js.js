104a5332e87c60df9e39ff7c0678735b269182ed
RailzGIT <kotinna561@gmail.com>
2025-02-15 01:56:26 +0300

    Test

---
+++ js.js
@@ -0,0 +1,8 @@
+function toggleAnswer(id) {
+    let answer = document.getElementById(id);
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
